0a5cf9132b5753f6343d5d5dd0b9ec40a1233711
aleckklt <aleckkouletio@gmail.com>
2025-10-24 16:45:16 +0100

    html gestion and add models.py

--- finance_droit/models.py
+++ finance_droit/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Conversation(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation de {self.user.username} le {self.created_at.strftime('%Y-%m-%d')}"
+
+class Message(models.Model):
+
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.CharField(max_length=20, choices=[('user', 'Utilisateur'), ('bot', 'IA')])
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message de {self.sender} : {self.text[:50]}"
