4a2ce8b5e73d2e7241845f55de3dca94119a0839
aleckklt <aleckkouletio@gmail.com>
2025-11-03 10:51:22 +0100

    end of the project1

--- finance_droit/utils.py
+++ finance_droit/utils.py
@@ -5,7 +5,7 @@ client = Client(host='http://localhost:11434')
 def ask_ollama(prompt):
 
     response = client.chat(
-        model="llama3.2:3b",
+        model="llama3",
         messages=[
             {
                 "role": "system",
