7ced46ab7c0301a203bbaceeec358bddb11b99ae
aleckklt <aleckkouletio@gmail.com>
2025-11-10 08:55:54 +0100

    end of the project

--- finance_droit/views.py
+++ finance_droit/views.py
@@ -16,7 +16,7 @@ def chatbot_view(request):
         user_message = data.get("message", "")
         if not user_message:
             return JsonResponse({"reply": "Veuillez entrer un message valide."})
-        history = Conversation.objects.order_by("-created_at")[:5][::-1]
+        history = Conversation.objects.order_by("created_at")
         bot_reply = ask_ollama(user_message, history)
         Conversation.objects.create(user_message=user_message, bot_reply=bot_reply)
         return JsonResponse({"reply": bot_reply})
